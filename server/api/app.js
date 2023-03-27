89ee24dfc2809b02c8a9c0d5f615888a3e3d70f0
Sujay Sankarapandian <palanikumarsankara.s@northeastern.edu>
2023-03-27 17:00:45 -0400

    Server code

--- server/api/app.js
+++ server/api/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
+mongoose.connect('mongodb://localhost:27017/colaco', {useNewUrlParser:true, useUnifiedTopology:true}); 
 // routes(app);
 
 export default app;
